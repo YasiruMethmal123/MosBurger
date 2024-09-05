832bf54a8aad44386e57dd46ef69f62e5342130e
Admin@UpaliLW <lalith.wijekoon@outlook.com>
2024-09-05 21:35:51 +0530

    MosBurger

---
+++ MyWeb.js
@@ -0,0 +1,436 @@
+
+
+const product = [
+  {
+    item_code: "B1001",
+    title: "Classic Burger (Large)",
+    price: "750.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Classic Burger (Large).jpg',
+  },
+  {
+    item_code: "B1002",
+    title: "Classic Burger (Regular)",
+    price: "1500.00",
+    discount: 15,
+    image: 'C:\my wEB\MosBurger\Burger\Classic Burger (Regular).jpg',
+  },
+  {
+    item_code: "B1003",
+    title: "Turkey Burger",
+    price: "1600.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Turkey Burger.jpg',
+  },
+  {
+    item_code: "B1004",
+    title: "Chicken Burger (Large)",
+    price: "1400.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Chicken Burger (Large).jpg',
+  },
+  {
+    item_code: "B1005",
+    title: "Chicken Burger (Regular)",
+    price: "800.00",
+    discount: 20,
+    image: 'C:\my wEB\MosBurger\Burger\Chicken Burger (Regular).jpeg',
+  },
+  {
+    item_code: "B1006",
+    title: "Cheese Burger (Large)",
+    price: "1000.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Cheese Burger (Large).jpg',
+  },
+  {
+    item_code: "B1007",
+    title: "Cheese Burger (Regular)",
+    price: "600.00",
+    discount: 0,
+    image: ',C:\my wEB\MosBurger\Burger\Cheese Burger (Regular).jpg',
+  },
+  {
+    item_code: "B1008",
+    title: "Bacon Burger",
+    price: "650.00",
+    discount: 15,
+    image: 'C:\my wEB\MosBurger\Burger\Bacon Burger.jpg',
+  },
+  {
+    item_code: "B1009",
+    title: "Shawarma Burger",
+    price: "800.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Shawarma Burger.jpg',
+  },
+  {
+    item_code: "B1010",
+    title: "Olive Burger",
+    price: "1800.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Olive Burger.jpg',
+  },
+  {
+    item_code: "B1012",
+    title: "Double-Cheese Burger",
+    price: "1250.00",
+    discount: 20,
+    image: 'C:\my wEB\MosBurger\Burger\Double-Cheese Burger.jpeg',
+  },
+  {
+    item_code: "B1013",
+    title: "Crispy Chicken Burger (Regular)",
+    price: "1200.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Crispy Chicken Burger (Regular).jpg',
+  },
+  {
+    item_code: "B1014",
+    title: "Crispy Chicken Burger (Large)",
+    price: "1600.00",
+    discount: 10,
+    image: 'C:\my wEB\MosBurger\Burger\Crispy Chicken Burger (Large).jpg',
+  },
+  {
+    item_code: "B1015",
+    title: "Paneer Burger",
+    price: "900.00",
+    discount: 0,
+    image: 'C:\my wEB\MosBurger\Burger\Paneer Burger.jpeg',
+  },
+  {
+    item_code: "B1016",
+    title: "Crispy Chicken <br>Submarine (Large)",
+    price: "2000.00",
+    discount: 0,
+    image: "topburger"
+  },
+  {
+    item_code: "B1017",
+    title: "Crispy Chicken <br>Submarine (Regular)",
+    price: "1500.00",
+    discount: 0,
+    image: "crispy_chicken_regular.jpg"
+  },
+  {
+    item_code: "B1018",
+    title: "Chicken Submarine (Large)",
+    price: "1800.00",
+    discount: 3,
+    image: "chicken_large.jpg"
+  },
+  {
+    item_code: "B1019",
+    title: "Chicken Submarine<br> (Regular)",
+    price: "1400.00",
+    discount: 0,
+    image: "chicken_regular.jpg"
+  },
+  {
+    item_code: "B1020",
+    title: "Grinder Submarine",
+    price: "2300.00",
+    discount: 0,
+    image: "grinder_submarine.jpg"
+  },
+  {
+    item_code: "B1021",
+    title: "Cheese Submarine",
+    price: "2200.00",
+    discount: 0,
+    image: "cheese_submarine.jpg"
+  },
+  {
+    item_code: "B1022",
+    title: "Double Cheese n<br> Chicken Submarine",
+    price: "1900.00",
+    discount: 16,
+    image: "double_cheese_chicken.jpg"
+  },
+  {
+    item_code: "B1023",
+    title: "Special Horgie Submarine",
+    price: "2800.00",
+    discount: 0,
+    image: "special_horgie.jpg"
+  },
+  {
+    item_code: "B1024",
+    title: "MOS Special Submarine",
+    price: "3000.00",
+    discount: 0,
+    image: "mos_special.jpg"
+  },
+  {
+    item_code: "B1025",
+    title: "Steak Fries (Large)",
+    price: "1200.00",
+    discount: 0,
+    image: "steak_fries_large.jpg"
+  },
+  {
+    item_code: "B1026",
+    title: "Steak Fries (Medium)",
+    price: "600.00",
+    discount: 0,
+    image: "steak_fries_medium.jpg"
+  },
+  {
+    item_code: "B1027",
+    title: "French Fries (Large)",
+    price: "800.00",
+    discount: 0,
+    image: "french_fries_large.jpg"
+  },
+  {
+    item_code: "B1028",
+    title: "French Fries (Medium)",
+    price: "650.00",
+    discount: 0,
+    image: "french_fries_medium.jpg"
+  },
+  {
+    item_code: "B1029",
+    title: "French Fries (Small)",
+    price: "450.00",
+    discount: 0,
+    image: "french_fries_small.jpg"
+  },
+  {
+    item_code: "B1030",
+    title: "Sweet Potato Fries (Large)",
+    price: "600.00",
+    discount: 0,
+    image: "sweet_potato_fries_large.jpg"
+  },
+  {
+    item_code: "B1031",
+    title: "Chicken n Cheese Pasta",
+    price: "1600.00",
+    discount: 15,
+    image: "chicken_cheese_pasta.jpg"
+  },
+  {
+    item_code: "B1032",
+    title: "Chicken Penne Pasta",
+    price: "1700.00",
+    discount: 0,
+    image: "chicken_penne_pasta.jpg"
+  },
+  {
+    item_code: "B1033",
+    title: "Ground Turkey Pasta Bake",
+    price: "2900.00",
+    discount: 10,
+    image: "turkey_pasta_bake.jpg"
+  },
+  {
+    item_code: "B1034",
+    title: "Creamy Shrimp Pasta",
+    price: "2000.00",
+    discount: 0,
+    image: "creamy_shrimp_pasta.jpg"
+  },
+  {
+    item_code: "B1035",
+    title: "Lemon Butter Pasta",
+    price: "1950.00",
+    discount: 0,
+    image: "lemon_butter_pasta.jpg"
+  },
+  {
+    item_code: "B1036",
+    title: "Tagliatelle Pasta",
+    price: "2400.00",
+    discount: 1,
+    image: "tagliatelle_pasta.jpg"
+  },
+  {
+    item_code: "B1037",
+    title: "Baked Ravioli",
+    price: "2000.00",
+    discount: 1,
+    image: "baked_ravioli.jpg"
+  },
+  {
+    item_code: "B1038",
+    title: "Fried Chicken (Small)",
+    price: "1200.00",
+    discount: 0,
+    image: "fried_chicken_small.jpg"
+  },
+  {
+    item_code: "B1039",
+    title: "Fried Chicken (Regular)",
+    price: "2300.00",
+    discount: 10,
+    image: "fried_chicken_regular.jpg"
+  },
+  {
+    item_code: "B1040",
+    title: "Fried Chicken (Large)",
+    price: "3100.00",
+    discount: 5,
+    image: "fried_chicken_large.jpg"
+  },
+  {
+    item_code: "B1041",
+    title: "Hot Wings (Large)",
+    price: "2400.00",
+    discount: 0,
+    image: "hot_wings_large.jpg"
+  },
+  {
+    item_code: "B1042",
+    title: "Devilled Chicken (Large)",
+    price: "900.00",
+    discount: 0,
+    image: "devilled_chicken_large.jpg"
+  },
+  {
+    item_code: "B1043",
+    title: "BBQ Chicken (Regular)",
+    price: "2100.00",
+    discount: 0,
+    image: "bbq_chicken_regular.jpg"
+  },
+
+  {
+    item_code: "B1044",
+    title: "Pepsi (330ml)",
+    price: "990.00",
+    discount: 5,
+    image: "pepsi_330ml.jpg"
+  },
+  {
+    item_code: "B1045",
+    title: "Coca-Cola (330ml)",
+    price: "1230.00",
+    discount: 0,
+    image: "coke_330ml.jpg"
+  },
+  {
+    item_code: "B1046",
+    title: "Sprite (330ml)",
+    price: "1500.00",
+    discount: 3,
+    image: "sprite_330ml.jpg"
+  },
+  {
+    item_code: "B1047",
+    title: "Mirinda (330ml)",
+    price: "850.00",
+    discount: 7,
+    image: "mirinda_330ml.jpg"
+  }
+]
+
+
+
+
+const categories = [...new Set(product.map((item) => { return item }))]
+
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+  const searchData = e.target.value.toLowerCase();
+  const filteredData = categories.filter((item) => {
+    return (
+      item.title.toLowerCase().includes(searchData)
+    )
+  })
+  displayItem(filteredData)
+});
+
+const displayItem = (items) => {
+  document.getElementById('root').innerHTML = items.map((item) => {
+    var { image, title, price } = item;
+    return (
+      ` <div class="col">
+                    <div class="card content" style="width: 18rem;">
+                        <img class='images' src=${image}></img>
+                        <div class="card-body">
+                            <p>${title}</p>
+                            <h5> LKR${price}.00</h5>
+                            
+                            <a href="#" class="btn btn-primary" style="border-radius: 20px; background-color: orangered;">Add to Cart</a>
+                        </div>
+                    </div>
+                    </div>`
+    )
+  }).join('')
+};
+displayItem(categories);
+
+
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
+
+function searchFunction() {
+  let searchValue = document.getElementById('searchInput').value;
+  if (searchValue) {
+    alert('You searched for: ' + searchValue);
+  } else {
+    alert('Please enter a search term.');
+  }
+}
+
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.title}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCards.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
